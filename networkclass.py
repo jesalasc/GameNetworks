0f7fd0c41c1df02f145a69c536018dbf3205180b
jesalasc <jusalas@uc.cl>
2021-11-26 10:27:32 +0000

    Create networkclass.py

---
+++ networkclass.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 26 09:58:06 2021
+
+@author: jesal
+"""
+import networkx as nx
+
+class Networks_Game:
+    
+    def __init__(self):
+        
+        
