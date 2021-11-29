434c762f3312566eea7c0daf61f3e546acaa2a67
jesalasc <jusalas@uc.cl>
2021-11-29 14:08:57 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,4 @@ Introduce your user id and password:
 
 import streamlit as st
 
-st.text_input("Enter your student ID", "", 8,"USER_ID", type="default", None,None, None, None, None, *, "s200****")
+st.text_input("Enter your student ID", "", 8,"USER_ID")
