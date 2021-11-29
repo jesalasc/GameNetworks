63b962850bb57008b4c8ce3145b17a76fe500fba
jesalasc <jusalas@uc.cl>
2021-11-29 14:08:02 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,15 +6,10 @@ Created on Fri Nov 26 10:28:14 2021
 """
 
 """
-# My first app
-Here's our first attempt at using data to create a table:
+# Game of networks
+Introduce your user id and password:
 """
 
 import streamlit as st
-import pandas as pd
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
 
-df
+st.text_input("Enter your student ID", "", 8,"USER_ID", type="default", None,None, None, None, None, *, "s200****")
