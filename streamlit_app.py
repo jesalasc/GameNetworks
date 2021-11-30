98056e6081908e13b3ff0338274f33aa61664071
jesalasc <jusalas@uc.cl>
2021-11-30 15:36:57 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ Created on Fri Nov 26 10:28:14 2021
 
 
 import streamlit as st
-#import networkclass as ntc
+import networkclass as ntc
 import json
 
 def check_identity(ID, PASSWORD):
@@ -20,35 +20,80 @@ def check_identity(ID, PASSWORD):
 
     if ID in Users.keys():
         if Users[ID] == PASSWORD:
-            return True
+            return True, Users.keys()
         else:
             st.write("Wrong Password")
-            return False
+            return False, None
     else:
         st.write("Wrong ID")
-        return False
-    return False
+        return False, None
+    return False, None
+
+def Init_Game(ID_List, Round):
+    students_list = []
+    for ID in ID_List:
+        students_list.append(ntc.Student(ID))
+    Game = ntc.Networks_Game(students_list,"Graph.txt")
+    return Game
 
 def main():
     
-    """
-    # Game of networks
-    Introduce your user id and password:
-    """
-  
+    if 'LogedIn' not in st.session_state:
+        st.session_state.LogedIn = False
+    
+    st.title("Game of Networks")
+    
+    
+    placeholder = st.empty()
+    if not st.session_state.LogedIn:
+        with placeholder.container():
+            "Introduce your user id and password:"
+            
+            USER_ID = st.text_input("Enter your student ID", "", 8,"USER_ID")
+            
+            PASSWORD = st.text_input("Enter your password", "", 8,"PASSWORD")
+            
+            LOGIN = st.button("ENTER", "Login_Button")
+            
+            if LOGIN:
+                attempt,ID_List = check_identity(USER_ID, PASSWORD)
+                if attempt:
+                    st.write("Log in successful")
+                    st.session_state.LogedIn = True
+                    with open('Metadata.txt') as f:
+                        data = f.read()
+                        Metadata = json.loads(data)
+                        f.close()
+                    st.sessionstate.Game = Init_Game(ID_List, Metadata["Round"])
+                    st.sessionstate.User = ntc.Student(USER_ID)
+                else:
+                    st.write("Log in failed")
+    
+    else:
+        placeholder.empty()
+        Round = st.sessionstate.Game.Round
+        st.title("Game of Networks Round: " + Round )
+        
+        if Round == 0:
+            Available = st.sessionstate.Game.unconnectedOF(st.sessionstate.User.id)
+            "Choose 2 connections to add"
+            "Connection 1"
+            add1 = st.selectbox("Add this connection", Available)
+            "Connection 2"
+            add2 = st.selectbox("Add this connection", Available)
+            st.buttom("SELECT", "add button")
 
-    
-    USER_ID = st.text_input("Enter your student ID", "", 8,"USER_ID")
-    
-    PASSWORD = st.text_input("Enter your password", "", 8,"PASSWORD")
-    
-    LOGIN = st.button("ENTER", "ButtonLogin","Login", check_identity, (USER_ID, PASSWORD))
-    
-    if LOGIN:
-        attempt = check_identity(USER_ID, PASSWORD)
-        if attempt:
-            st.write("Login successful")
+        
         else:
-            st.write("Login failed")
-
+            Available = st.sessionstate.Game.unconnectedOF(st.sessionstate.User.id)
+            Connected = st.sessionstate.Game.connectionsOF(st.sessionstate.User.id)
+            "Choose 2 connections to add and one connection to remove"
+            "Connection 1"
+            add1 = st.selectbox("Add this connection", Available)
+            "Connection 2"
+            add2 = st.selectbox("Add this connection", Available)
+            "Remove Connection"
+            rem = st.selectbox("Add this connection", Connected)
+            st.buttom("SELECT", "add button")
+        
 main()
